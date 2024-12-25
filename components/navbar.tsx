c828f609b15b0622c514d1a6276bf25fedac1113
Evandabest <evanhaque1@gmail.com>
2024-12-25 00:20:28 -0500

    fix: update navbar and include projects

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,17 +4,41 @@ import Link from "next/link"
 const dataStructuresItems = [
   { href: "/arrays", label: "Arrays" },
   { href: "/linkedlist", label: "Linked Lists" },
-  { href: "/trees", label: "Trees" },
+  { href: "/binary", label: "Binary Trees" },
   { href: "/graphs", label: "Graphs" },
+  { href: "/stack", label: "Stacks" },
+  { href: "/queue", label: "Queues" },
+  { href: "/heap", label: "Heaps" },
   { href: "/hash", label: "Hash Tables" },
 ]
 
 const algorithmItems = [
   { href: "/sorting/bubble", label: "Bubble Sort" },
+  { href: "/sorting/quick", label: "Quick Sort" },
   { href: "/sorting/insertion", label: "Insertion Sort" },
   { href: "/sorting/selection", label: "Selection Sort" },
   { href: "/sorting/merge", label: "Merge Sort" },
+  { href: "/traversal/bfs", label: "Breadth First Search" },
+  { href: "/traversal/dfs", label: "Depth First Search" },
+  { href: "/dijkstras", label: "Dijkstra's Algorithm" },
+  { href: "/dijkstras/bellmonford", label: "Bellman-Ford Algorithm" },
+  { href: "/recursion", label: "Recursion" },
+
 ]
+
+const ProjectItems = [
+  { href: "/vector", label: "Vector" },
+  { href: "/bag", label: "Bag" },
+  { href: "/sequence", label: "Sequence" },
+  { href: "/statistician", label: "Statistician" },
+  { href: "/set", label: "Set" },
+  { href: "/stack", label: "Stack" },
+  { href: "/queue", label: "Queue" },
+  { href: "/binary", label: "Binary Tree" },
+  { href: "/bst", label: "Binary Search Tree" },
+  { href: "/heap", label: "Heap" }
+]
+
 
 const Navbar = () => {
   return (
@@ -32,6 +56,7 @@ const Navbar = () => {
           <div className="flex-1 flex items-center justify-end space-x-8">
             <HoverDropdown trigger="Data Structures" items={dataStructuresItems} />
             <HoverDropdown trigger="Algorithms" items={algorithmItems} />
+            <HoverDropdown trigger="Projects" items={ProjectItems} />
             <Link href="/code">Code</Link>
           </div>
         </div>
